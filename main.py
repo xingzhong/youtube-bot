1f02122d9372a502fc629461908ef32bf077fe5a
Xingzhong <xu.xeno@gmail.com>
2014-11-28 13:14:27 -0500

    update

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import glob
 import os
 from uploadVideo import get_authenticated_service, initialize_upload
+from oauth2client.tools import argparser, run_flow
 
 if __name__ == '__main__':
 	for fn in glob.glob('./video/*.flv'):
-		args = {}
-		args['file'] = fn
-		args['title'] = fn.split('/')[-1][:-4]
+		argparser.add_argument("--file", default=fn)
+		argparser.add_argument("--title", default=fn.split('/')[-1][:-4])
+		args = argparser.parse_args()
 		if not os.path.exists(args.file):
 			exit("Please specify a valid file using the --file= parameter.")
+		break
 		youtube = get_authenticated_service(args)
 		try:
 			initialize_upload(youtube, args)
